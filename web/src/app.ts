2417bdce91499ecc2c47c8dbad3201ad148cdfdf
madao <goomadao@gmail.com>
2020-03-07 16:09:15 +0800

    Improvements and finish web

--- web/src/app.ts
+++ web/src/app.ts
@@ -2,10 +2,7 @@ export const dva = {
   config: {
     onError(err: ErrorEvent) {
       err.preventDefault();
-      console.error(err.message);
     },
   },
-  plugins: [
-    require('dva-logger')(),
-  ],
+  plugins: [require('dva-logger')()],
 };
